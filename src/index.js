9bad1290c4692f78f73678f4d31f74f912fc0296
Andrew <milnera92@outlook.com>
2023-01-12 19:52:01 -0500

    disable dev tools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
-
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import App from "./App";
 
 // make env for auth0
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
